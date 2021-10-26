741272d516a7e96e4ed8048bb50b4d9d448c9628
Anthony Yan <anthonyyan@Anthonys-MacBook-Pro.local>
2021-10-26 16:43:07 -0500

    added components/pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import {useRoutes, BrowserRouter, Route} from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Program from "./pages/Program"
+import Join from "./pages/Join"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const routes = [
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "programs",
+      element: <Program />
+    },
+    {
+      path: "/join",
+      element: <Join />
+    }
+  ]
+  return useRoutes(routes);
 }
 
 export default App;
