c3930536a72ca5b94a7d268374f9dd6f4d3178fa
Anthony Yan <ady2@rice.edu>
2022-01-16 21:45:31 -0600

    added slideshow + section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,35 @@ import React from 'react'
 import Layout from "../layouts/Layout.js"
 import Header from "../components/shared/Header.js"
 import Blurb from "../components/shared/Blurb.js"
+import SlideShow from "../components/home/Slideshow.js"
 import "../styles/general.css"
 import "../styles/Home.css"
 
 const Home = () => {
 
-    const welcomeHeader = "Welcome to Rice DataSci!"
+    const headline = "something catchy"
     const aboutBlurb = "We're a community of undergrads studying and practicing data science at Rice!"
     const interestBlurb = "Check out some of our recent events, learn more about us, or join!"
     
     return (
         <Layout>
             <div className="homeContainer">
-                {/* insert slideshow */}
-                <div className="slideShow"> 
-                    slideshow
+                {/* first section */}
+                <div className="firstSection">
+                    <div className="firstSectionBio">
+                        <Header title={headline} />
+                        <Blurb description={aboutBlurb} horizontalPadding={5}/>
+                    </div>
+                    {/* insert slideshow */}
+                    <div className="slideShowContainer"> 
+                        <div className="slideShow">
+                            <SlideShow />
+                        </div>
+                    </div>
                 </div>
-                <Header title={welcomeHeader} />
-                <Blurb description={aboutBlurb} horizontalPadding={5}/>
-                <Blurb description={interestBlurb} horizontalPadding={5}/>
+                
+                
+                {/* <Blurb description={interestBlurb} horizontalPadding={5}/> */}
             </div>
         </Layout>
 
